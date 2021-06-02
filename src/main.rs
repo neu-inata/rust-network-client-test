84b81dce6d7d8e96052305a01d99608accdc131c
neu-inata <sironeu@yahoo.co.jp>
2021-06-02 23:14:17 +0900

    feat #--- 他スレッドでstreamを初期化するよう変更

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,53 @@
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{self, Write};
 use std::net::TcpStream;
-use std::str;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
+use std::mem;
 
 fn main() {
-    let mut stream: TcpStream;
-
-    loop {
+    let stream: TcpStream;
+    unsafe{
+        stream = mem::uninitialized();
+    }
+    let stream = Arc::new(Mutex::new(stream));
+    let stream_clone = stream.clone();
+    let handle = thread::spawn(move || loop {
         let ip = "127.0.0.1:7777".parse().unwrap();
         let result = TcpStream::connect_timeout(&ip, Duration::from_secs(1));
         match result {
-            Ok(tcpStream) => {
-                stream = tcpStream;
+            Ok(stream) => {
+                let mut s = stream_clone.lock().unwrap();
+                *s = stream;
                 break;
             }
-            Err(err) => {
-                println!("connecting")
+            Err(_) => {
+                println!("connecting");
             }
         }
-    }
+    });
+    let _ = handle.join();
 
+    let mut stream_mut = stream.as_ref().lock().unwrap();
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        let result = stream.write(input.as_bytes());
+        let result = stream_mut.write(input.as_bytes());
         match result {
-            Ok(sz) => { 
+            Ok(sz) => {
                 if sz == 0 {
                     break;
                 }
-            },
-            Err(err) => {}
+            }
+            Err(_) => {}
         }
     }
 
-    match stream.shutdown(std::net::Shutdown::Both) {
+    match stream_mut.shutdown(std::net::Shutdown::Both) {
         Err(err) => {
             println!("{}", err.to_string());
             panic!();
-        },
+        }
         _ => {}
     }
 }
